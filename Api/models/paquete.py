04dd0486bef1424015dec22182156f6511ed09e2
Zea <80776867+Tuma2k@users.noreply.github.com>
2025-10-20 10:44:48 -0300

    Actualizar paquete.py

--- Api/models/paquete.py
+++ Api/models/paquete.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel
-from datetime import datetime 
+from datetime import datetime, date, time # <-- IMPORTAMOS DATE Y TIME
 from typing import List, Any 
 
+# (pa_reception y respuesta se quedan igual)
 class pa_reception(BaseModel):
     Humedad: float
     Temperatura: float
@@ -11,11 +12,14 @@ class respuesta(BaseModel):
     respuesta: str
     fechahora: datetime
 
+# --- CAMBIOS AQUÍ ---
+
 class Registro(BaseModel):
     Humedad: float
     Temperatura: float
     EstadoBoton: int
-    instantelectura: datetime 
+    fecha: date # <-- Columna de fecha
+    hora: time  # <-- Columna de hora
 
 class respGet(BaseModel):
-    registros: List[Registro]
+    registros: List[Registro] # Usa el nuevo modelo 'Registro'
